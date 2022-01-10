3cab0c2d341a944dc77a86d9330d92019e410948
virsp <victor.pettersson@gmail.com>
2022-01-10 22:43:58 +0100

    added bootstrap-icons

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -4,6 +4,11 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+import { BootstrapVueIcons } from 'bootstrap-vue'
+import 'bootstrap-vue/dist/bootstrap-vue-icons.min.css'
+
+Vue.use(BootstrapVueIcons)
+
 Vue.config.productionTip = false
 
 new Vue({
