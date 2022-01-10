d37d05e1d27ca17e0bcb18446a234b32e525ea32
virsp <victor.pettersson@gmail.com>
2022-01-10 22:44:30 +0100

    basic dashboard for testing

---
+++ frontend/src/views/Dashboard.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="row pt-2">
+        <div class="col">
+            <h1>Shelly overview</h1>
+            <Shelly/>
+        </div>
+    </div>
+</template>
+<script>
+import Shelly from '../components/Shelly.vue';
+export default {
+    name: 'Dashboard',
+    components: {
+        Shelly,
+    },
+}
+</script>
+<style>
+    
+</style>
