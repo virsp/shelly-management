78414bb7663a8513a1e3de7467e615e10825b3ab
virsp <victor.pettersson@gmail.com>
2022-01-10 22:44:14 +0100

    added dashboard routing

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -19,6 +19,13 @@ const routes = [
     component: function () {
       return import(/* webpackChunkName: "about" */ '../views/About.vue')
     }
+  },
+  {
+    path: '/dashboard',
+    name: 'Dashboard',
+    component: function () {
+      return import('../views/Dashboard.vue')
+    }
   }
 ]
 
