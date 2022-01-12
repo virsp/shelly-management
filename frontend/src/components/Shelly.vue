8711484e52ec74787d740047cd7588363ca59982
virsp <victor.pettersson@gmail.com>
2022-01-12 20:14:51 +0100

    rotating more/less button, added more status field

--- frontend/src/components/Shelly.vue
+++ frontend/src/components/Shelly.vue
@@ -3,12 +3,13 @@
         <div class="col-auto">
             <b-card v-bind:title="settings.device.type + ' - ' + settings.name" border-variant="info">
                 <b-card-text>
+					<!-- WIFI AT -->
                     <div class="mb-0 ">
                         <div class="text-left">
-                            <b-icon id="clickable" ref="click" @click="showMore(0)" icon="chevron-double-down"></b-icon> Wifi AP - <b v-bind:class="{'text-success': settings.wifi_ap.enabled, 'text-danger': !settings.wifi_ap.enabled}">{{settings.wifi_ap.enabled}}</b>
-                    
+							<b-icon v-if="this.toggle[0]" id="clickable" ref="click" @click="showMore(0)" icon="chevron-double-down"></b-icon>
+							<b-icon v-else id="clickable" ref="click" @click="showMore(0)" icon="chevron-double-right"></b-icon>
+                             Wifi AP - <b v-bind:class="{'text-success': settings.wifi_ap.enabled, 'text-danger': !settings.wifi_ap.enabled}">{{settings.wifi_ap.enabled}}</b>
                         </div>
-                        <!-- <b-button @click="checkVerbose()"></b-button> -->
                         <div v-if="this.toggle[0]">
                             <div class="row pl-4" v-for="(value, name) in removeEnable(settings.wifi_ap)" v-bind:key="value">
                                 <div class="text-capitalize">{{name}}</div>: {{value}}
@@ -17,9 +18,12 @@
                         </div>
                     </div>
 
+					<!-- WIFI STA -->
                     <div class="mb-0">
                         <div class="text-left">
-                            <b-icon id="clickable" ref="click" @click="showMore(1)" icon="chevron-double-down"></b-icon> Wifi STA - <b v-bind:class="{'text-success': settings.wifi_sta.enabled, 'text-danger': !settings.wifi_sta.enabled}">{{settings.wifi_sta.enabled}}</b>
+                            <b-icon v-if="this.toggle[1]" id="clickable" ref="click" @click="showMore(1)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(1)" icon="chevron-double-right"></b-icon> 
+							Wifi STA - <b v-bind:class="{'text-success': settings.wifi_sta.enabled, 'text-danger': !settings.wifi_sta.enabled}">{{settings.wifi_sta.enabled}}</b>
                         </div>
 
                         <div v-if="this.toggle[1]">
@@ -29,15 +33,78 @@
                         </div>
                     </div>
                     
+					<!-- MQTT -->
                     <div class="mb-0">
                         <div class="text-left">
-                            <b-icon id="clickable" ref="click" @click="showMore(1)" icon="chevron-double-down"></b-icon> MQTT - <b v-bind:class="{'text-success': settings.mqtt.enable, 'text-danger': !settings.mqtt.enable}">{{settings.mqtt.enable}}</b>
+                            <b-icon v-if="this.toggle[2]" id="clickable" ref="click" @click="showMore(2)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(2)" icon="chevron-double-right"></b-icon> 
+							MQTT - <b v-bind:class="{'text-success': settings.mqtt.enable, 'text-danger': !settings.mqtt.enable}">{{settings.mqtt.enable}}</b>
                         </div>
-                        <div v-if="this.toggle[2]">
+                        
+						<div v-if="this.toggle[2]">
                             <div class="row pl-4" v-for="(value, name) in removeEnable(settings.mqtt)" v-bind:key="name">
                                 <div class="text-capitalize">{{name}}</div>: {{value}}
                             </div>
+                        </div>
+                    </div> 
 
+					<!-- CoIoT -->
+                    <div class="mb-0">
+                        <div class="text-left">
+                            <b-icon v-if="this.toggle[3]" id="clickable" ref="click" @click="showMore(3)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(3)" icon="chevron-double-right"></b-icon> 
+							CoIoT - <b v-bind:class="{'text-success': settings.coiot.enabled, 'text-danger': !settings.coiot.enabled}">{{settings.coiot.enabled}}</b>
+                        </div>
+                        
+						<div v-if="this.toggle[3]">
+                            <div class="row pl-4" v-for="(value, name) in removeEnable(settings.coiot)" v-bind:key="name">
+                                <div class="text-capitalize">{{name}}</div>: {{value}}
+                            </div>
+                        </div>
+                    </div> 
+
+					<!-- SNTP -->
+                    <div class="mb-0">
+                        <div class="text-left">
+                            <b-icon v-if="this.toggle[4]" id="clickable" ref="click" @click="showMore(4)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(4)" icon="chevron-double-right"></b-icon> 
+							SNTP - <b v-bind:class="{'text-success': settings.sntp.enabled, 'text-danger': !settings.sntp.enabled}">{{settings.sntp.enabled}}</b>
+                        </div>
+                        
+						<div v-if="this.toggle[4]">
+                            <div class="row pl-4" v-for="(value, name) in removeEnable(settings.sntp)" v-bind:key="name">
+                                <div class="text-capitalize">{{name}}</div>: {{value}}
+                            </div>
+                        </div>
+                    </div> 
+
+					<!-- Login -->
+                    <div class="mb-0">
+                        <div class="text-left">
+                            <b-icon v-if="this.toggle[5]" id="clickable" ref="click" @click="showMore(5)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(5)" icon="chevron-double-right"></b-icon> 
+							Login - <b v-bind:class="{'text-success': settings.login.enabled, 'text-danger': !settings.login.enabled}">{{settings.login.enabled}}</b>
+                        </div>
+                        
+						<div v-if="this.toggle[5]">
+                            <div class="row pl-4" v-for="(value, name) in removeEnable(settings.login)" v-bind:key="name">
+                                <div class="text-capitalize">{{name}}</div>: {{value}}
+                            </div>
+                        </div>
+                    </div>
+
+					<!-- CLOUD -->
+                    <div class="mb-0">
+                        <div class="text-left">
+                            <b-icon v-if="this.toggle[6]" id="clickable" ref="click" @click="showMore(6)" icon="chevron-double-down"></b-icon> 
+                            <b-icon v-else id="clickable" ref="click" @click="showMore(6)" icon="chevron-double-right"></b-icon> 
+							Cloud - <b v-bind:class="{'text-success': settings.cloud.enabled, 'text-danger': !settings.cloud.enabled}">{{settings.cloud.enabled}}</b>
+                        </div>
+                        
+						<div v-if="this.toggle[6]">
+                            <div class="row pl-4" v-for="(value, name) in removeEnable(settings.cloud)" v-bind:key="name">
+                                <div class="text-capitalize">{{name}}</div>: {{value}}
+                            </div>
                         </div>
                     </div> 
                    
