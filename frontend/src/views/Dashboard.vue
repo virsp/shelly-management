9eaccb21015ae0ecd6dc5c67b45c4719c89984f4
virsp <victor.pettersson@gmail.com>
2022-01-13 21:22:37 +0100

    basic dashboard implemented

--- frontend/src/views/Dashboard.vue
+++ frontend/src/views/Dashboard.vue
@@ -2,16 +2,37 @@
     <div class="row pt-2">
         <div class="col">
             <h1>Shelly overview</h1>
-            <Shelly/>
+            <div v-if="this.shellies" class="row">
+                <div v-for="shelly in this.shellies" class="col-auto">
+                    <Shelly :ip="shelly"/>
+                </div>
+            </div>
         </div>
     </div>
 </template>
 <script>
-import Shelly from '../components/Shelly.vue';
+import Shelly from '@/components/Shelly.vue';
+import {db} from '@/api/database';
+import logger from '@/plugins/logger';
 export default {
     name: 'Dashboard',
     components: {
         Shelly,
+    },
+    data() {
+        return {
+            shellies: null,
+        }
+    },
+    methods: {
+        async getDeviceIPS(){
+            let temp = await db.getIPS();
+            this.shellies = temp.ips;
+            logger.log(this.shellies);
+        }
+    },
+    mounted() {
+        this.getDeviceIPS();
     },
 }
 </script>
