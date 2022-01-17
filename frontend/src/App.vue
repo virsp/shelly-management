ca25f891719a22321ae453beca4e06d972207782
virsp <victor.pettersson@gmail.com>
2022-01-17 16:39:14 +0100

    removed example components

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -5,7 +5,6 @@
        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
        <b-collapse id="nav-collapse" is-nav>
          <b-navbar-nav>
-           <b-nav-item><router-link tag="li" to="/home">Home</router-link></b-nav-item>
            <b-nav-item><router-link tag="li" to="/about">About</router-link></b-nav-item>
            <b-nav-item><router-link tag="li" to="/dashboard">Dashboard</router-link></b-nav-item>
          </b-navbar-nav>
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,15 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
-  },
   {
     path: '/about',
     name: 'About',
@@ -21,7 +15,7 @@ const routes = [
     }
   },
   {
-    path: '/dashboard',
+    path: '/',
     name: 'Dashboard',
     component: function () {
       return import('../views/Dashboard.vue')
