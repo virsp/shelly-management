a87a9a4bec3df36d8387f667662d9512d50f3475
virsp <victor.pettersson@gmail.com>
2022-01-26 21:38:47 +0100

    ips stored in vuex now

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -1,15 +1,39 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-
-Vue.use(Vuex)
+import Vue from 'vue';
+import Vuex from 'vuex';
+import { db } from '@/api/database';
+import logger from '@/plugins/logger';
+Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+    state: {
+        ips: []
+    },
+    mutations: {
+        saveIPS (state, ips) {
+            state.ips = ips;
+        }
+    },
+    actions: {
+        /**
+         * Retrieves the ips from the database
+         * error handling not implemented yet
+         * @param {*} state
+         */
+        async getIPS (state) {
+            try {
+                const ips = await db.getIPS();
+                logger.log('Saving ips to store');
+                state.commit('saveIPS', ips);
+            } catch (error) {
+                logger.error(error);
+            }
+        }
+    },
+    getters: {
+        getIPS (state) {
+            return state.ips;
+        }
+    },
+    modules: {
+    }
+});
