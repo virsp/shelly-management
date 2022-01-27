6b64419f5b903c165549dd13f0037ee7c2c7561f
virsp <victor.pettersson@gmail.com>
2022-01-27 21:17:42 +0100

    uses Set() to handle duplicates

--- backend/data.js
+++ backend/data.js
@@ -3,25 +3,37 @@ const file = "ips.json";
 let shellies;
 
 try {
-    shellies = require('./ips.json');    
+    let imported = require('./ips.json');
+    shellies = new Set(imported);    
 } catch (error) {
     console.log('No data file found, init new');
-    shellies = {
-        ips: []
-    }
+    shellies = new Set();
 }
 
+/**
+ * Adds the ip to the database
+ * if ip already exists it throws an error 
+ * @param {String} ip to be added to the database
+ */
 async function addIP(ip){
-    shellies.ips.push(ip);
+    if(shellies.has(ip)){
+        console.log(ip + " already exists!");
+        throw new Error(ip + ' already exists!');
+    }
+    shellies.add(ip)
     try {
         console.log('Writing ' + ip + ' to file');
-        fs.writeFileSync(file, JSON.stringify(shellies));
+        fs.writeFileSync(file, JSON.stringify(Array.from(shellies)));
     } catch (error) {
-        console.log(error);
+        console.log("file write error " + error);
+        throw new Error('Something went wrong writing to file: ' + error);
     }
 }
-
+/**
+ * Retrieve stored ips
+ * @returns an object with the ips with the format {ips: [...]}
+ */
 function getIPS(){
-    return shellies;
+    return {ips: Array.from(shellies)};
 }
 module.exports = {addIP, getIPS};
