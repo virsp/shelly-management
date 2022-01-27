e19639aba73ea28d29323a46ddc122834c19867c
virsp <victor.pettersson@gmail.com>
2022-01-27 20:48:39 +0100

    add api endpoint

--- frontend/src/api/database.js
+++ frontend/src/api/database.js
@@ -14,6 +14,16 @@ const db = {
             })
             .catch(err => logger.error('error:' + err));
         return data;
+    },
+    addIP: async (ip) => {
+        const url = process.env.VUE_APP_DATABASE + '/add?ip=' + ip;
+
+        const options = { method: 'POST' };
+
+        fetch(url, options)
+            .then(res => res.json())
+            .then(json => console.log(json))
+            .catch(err => console.error('error:' + err));
     }
 };
 
