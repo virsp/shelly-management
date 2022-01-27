0ae3fb0823429eba709ecb56099646e49e563c0f
virsp <victor.pettersson@gmail.com>
2022-01-27 21:33:30 +0100

    remove ip, and file save seperation

--- backend/data.js
+++ backend/data.js
@@ -11,6 +11,19 @@ try {
 }
 
 /**
+ * Saves the state to file
+ */
+async function saveToFile(){
+    try {
+        console.log('Saving to file...');
+        fs.writeFileSync(file, JSON.stringify(Array.from(shellies)));
+    } catch (error) {
+        console.log("File write error " + error);
+        throw new Error('Something went wrong writing to file: ' + error);
+    }
+}
+
+/**
  * Adds the ip to the database
  * if ip already exists it throws an error 
  * @param {String} ip to be added to the database
@@ -20,20 +33,36 @@ async function addIP(ip){
         console.log(ip + " already exists!");
         throw new Error(ip + ' already exists!');
     }
-    shellies.add(ip)
     try {
-        console.log('Writing ' + ip + ' to file');
-        fs.writeFileSync(file, JSON.stringify(Array.from(shellies)));
+        shellies.add(ip)
+        await saveToFile();
     } catch (error) {
-        console.log("file write error " + error);
-        throw new Error('Something went wrong writing to file: ' + error);
+        throw error;
     }
 }
+
 /**
  * Retrieve stored ips
  * @returns an object with the ips with the format {ips: [...]}
  */
 function getIPS(){
     return {ips: Array.from(shellies)};
+}
+
+/**
+ * Removes the given ip from the database
+ * if ip already exists it throws error 
+ * @param {String} ip to be removed
+ */
+function removeIP(ip){
+    if(!shellies.has(ip)){
+        throw new Error(ip + 'does not exist in the db');
+    }
+    try {
+        shellies.delete(ip);
+        await saveToFile();
+    } catch (error) {
+        throw error;
+    }
 }
 module.exports = {addIP, getIPS};
