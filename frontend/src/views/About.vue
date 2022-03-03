fde870a21073c7844c8464107eca065b22fa404d
virsp <victor.pettersson@gmail.com>
2022-03-03 23:14:34 +0100

    added some info

--- frontend/src/views/About.vue
+++ frontend/src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="container">
+    <div class="row">
+      <h1 class="col">About Shelly Management</h1>
+    </div>
+    <div class="row justify-content-md-center">
+      <div class="col-5">
+        <p>
+          This web interface was developed in order to have a centralized view of shelly devices on a network,
+          for now it features updating and basic information. But the intention is to introduce auto-discovery
+          and more configuration options.
+        </p>
+        <p>
+          For more information about shellies api visit
+          <a href="https://shelly-api-docs.shelly.cloud/">Shelly API Documentation</a>.
+        </p>
+      </div>
+    </div>
   </div>
 </template>
